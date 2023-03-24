34daafd1308c184b9d261bc1ba954a7ecc16be56
Rubesh <Rubeshnath>
2023-03-24 22:31:47 +0530

    seperate test cases

--- cypress/e2e/Tests/loginTest.cy.js
+++ cypress/e2e/Tests/loginTest.cy.js
@@ -8,25 +8,32 @@ context('Login tests', () => {
     })
     
     
-    it('.type() - type into a DOM element', () => {
+    it('Verify login', () => {
         
         loginPage.signIn()
-        
-        //cy.get('input[placeholder="Username or Email"]').type('vraghul0001@gmail.com')
-        //cy.get('input[placeholder="Password"]').type('Abcd@123')
-        //cy.get('#btnNext').click()
+         
+    })
+
+    it('verify user details', () =>{
+        loginPage.signIn()
         cy.wait(5000)
         if(cy.contains('Go to Dashboard')){
         cy.get('button[class="btn min-width btn-danger"]').click()
         }
         cy.xpath('//div[@class="user-title justify-content-start mt-3"]').should('contain', 'Welcome')
         cy.xpath('//div[@class="user-title justify-content-start mt-3"]').should('contain', 'Client ID')
+    })
+
+    it('Verify withdrawal button', () => {
+        loginPage.signIn()
+        cy.wait(5000)
+        if(cy.contains('Go to Dashboard')){
+        cy.get('button[class="btn min-width btn-danger"]').click()
+        }
         cy.xpath('//button[normalize-space()="Withdrawal"]').should('have.class','btn btn-lg min-width btn-solid-navigation mx-1')
         .should('contain', 'Withdrawal')
         .should('be.visible')
         .should('be.enabled')
         .click()
-
-    
     })
 })
