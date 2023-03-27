89eae221d861c92349a8ce46e45a245fd0e86820
Rubesh <Rubeshnath>
2023-03-27 11:29:19 +0530

    mochawesome reporter added

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
+  "reporter": "junit",
+ 
+    "reporterOptions": {
+ 
+       "mochaFile": "cypress/results/results-[hash].xml",
+ 
+       "toConsole": true
+ 
+    },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require("cypress-mochawesome-reporter/Plugin")(on);
+      
     },
   },
 });
